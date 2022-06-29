029b3aff390a015ecd7dad65ab5c84a33ff11e34
nanditaupreti <nanditaupreti312@gmail.com>
2022-06-29 13:35:03 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Home from "./pages/home/home";
+import Login from "./pages/login/login"
+import Register from "./pages/register/register"
+import Profile from "./pages/Profile/profile"
+function App() {
+  return (
+    <div >
+      {/* <Register/>
+     <Login/>  */}
+     <Home />
+     {/* <Profile/> */}
+    </div>
+  );
+}
+
+export default App;
